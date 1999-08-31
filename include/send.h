411b5ed36941eb1a4bce2b6be7c0a16bd52c075a
wnder <wnder>
1999-08-31 19:57:33 +0000

    standard gnu copyright

--- include/send.h
+++ include/send.h
@@ -1,8 +1,22 @@
-/*
- * send.h
- * Copyright (C) 1999 Patrick Alken
+/************************************************************************
  *
- * $Id: send.h,v 7.0 1999/08/01 21:19:46 lusky Exp $
+ *   IRC - Internet Relay Chat, include/send.h
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 1, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id: send.h,v 7.1 1999/08/31 19:57:33 wnder Exp $
  */
 
 #ifndef INCLUDED_send_h
