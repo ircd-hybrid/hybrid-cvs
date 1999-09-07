c12618ba260bd144c40de14c458387754cb3457f
tomh <tomh>
1999-09-07 04:43:10 +0000

    read dbufs right into client buffer

--- include/packet.h
+++ include/packet.h
@@ -19,7 +19,7 @@
  *
  * "packet.h". - Headers file.
  *
- * $Id: packet.h,v 7.1 1999/09/03 05:05:34 tomh Exp $
+ * $Id: packet.h,v 7.2 1999/09/07 04:43:10 tomh Exp $
  *
  */
 #ifndef INCLUDED_packet_h
@@ -32,7 +32,7 @@
 struct Client;
 
 extern int dopacket(struct Client* client, char* buf, size_t len);
-extern int client_dopacket(struct Client* client, char* buf, size_t len);
+extern int client_dopacket(struct Client* client, size_t len);
 
 #endif /* INCLUDED_packet_h */
 
