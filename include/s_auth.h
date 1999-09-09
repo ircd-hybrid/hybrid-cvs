4d1da1f717f7b59d355c36c9b53d088d7cb352b6
wnder <wnder>
1999-09-09 13:40:04 +0000

    more #ifdef USE_IAUTH stuff

--- include/s_auth.h
+++ include/s_auth.h
@@ -15,7 +15,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: s_auth.h,v 7.0 1999/08/01 21:19:46 lusky Exp $
+ *   $Id: s_auth.h,v 7.1 1999/09/09 13:40:04 wnder Exp $
  */
 #ifndef INCLUDED_s_auth_h
 #define INCLUDED_s_auth_h
@@ -65,11 +65,14 @@ struct AuthRequest {
 
 
 extern struct AuthRequest* AuthPollList;  /* GLOBAL - auth queries pending io */
+extern struct AuthRequest* AuthClientList;
 
 extern void start_auth(struct Client *);
 extern void timeout_auth_queries(time_t now);
 extern void read_auth_reply(struct AuthRequest* req);
 extern void send_auth_query(struct AuthRequest* req);
+extern void remove_auth_request(struct AuthRequest *req);
+extern struct AuthRequest *FindAuthClient(long id);
 
 #endif /* INCLUDED_s_auth_h */
 
