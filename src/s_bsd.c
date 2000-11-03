e362fb2f93e24c5bc817bd99c245ea34f5dface5
adrian <adrian>
2000-11-03 22:35:46 +0000

    with REJECT_HOLD, we read() from the fd to flush the buffer, instead of ignoring it. This is event-IO friendly, and it will stop triggering unwanted read IO events.

--- src/s_bsd.c
+++ src/s_bsd.c
@@ -17,7 +17,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *  $Id: s_bsd.c,v 7.57 2000/11/03 22:17:42 adrian Exp $
+ *  $Id: s_bsd.c,v 7.58 2000/11/03 22:35:46 adrian Exp $
  */
 #include "fdlist.h"
 #include "s_bsd.h"
@@ -504,6 +504,9 @@ read_packet(int fd, void *data)
   struct Client *cptr = data;
   int length = 0;
   int done;
+#ifdef REJECT_HOLD
+  static char buf[8192];
+#endif
 
   /*
    * Check for a dead connection here. This is done here for legacy
@@ -549,10 +552,9 @@ read_packet(int fd, void *data)
    * FLAGS_REJECT_HOLD should NEVER be set for non local client 
    */
   if (IsRejectHeld(cptr)) {
-    /*
-     * XXX I don't like this! We should probably read to flush the
-     * socket buffer .. -- adrian */
-     */
+    /* Flush the socket buffer so event IO systems don't go nuts -- adrian */
+    if (read(fd, buf, 8191) < 0)
+      error_exit_client(cptr, -1); /* whee .. */
     goto finish;
   }
 #endif
