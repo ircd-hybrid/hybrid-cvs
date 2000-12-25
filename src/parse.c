56bc63011909f88a44fa4f8d16e485273f10232f
db <db>
2000-12-25 05:54:41 +0000

    - ummmm parse isnt' handed a null terminated line, its handed the end of   of the buffer as well as the start. The end of the buffer if its   rfc compliant, will be '\r' '\n' but no terminating '\0'   so, take bufend look one back that should be a '\n', then blast it   then look to see if there is a '\r' before it, if so blast it as well   then thats it... end is now the valid end of the buffer

--- src/parse.c
+++ src/parse.c
@@ -17,7 +17,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: parse.c,v 7.61 2000/12/25 05:06:33 db Exp $
+ *   $Id: parse.c,v 7.62 2000/12/25 05:54:41 db Exp $
  */
 #include "parse.h"
 #include "client.h"
@@ -72,6 +72,7 @@ int parse(struct Client *cptr, char *buffer, char *bufend)
   struct Client*  from = cptr;
   char*           ch;
   char*           s;
+  char*           end;
   int             i;
   int             paramcount, mpara;
   int             handle_idx = -1;	/* Handler index */
@@ -234,6 +235,11 @@ int parse(struct Client *cptr, char *buffer, char *bufend)
 
   i = 1;
 
+  end = bufend-1;
+
+  if(*end == '\n')*end-- = '\0';
+  if(*end == '\r')*end = '\0';
+
   if (s)   /* redone by is, aug 2000 */
   {
       if (paramcount > MAXPARA)
@@ -250,7 +256,7 @@ int parse(struct Client *cptr, char *buffer, char *bufend)
 				  while (*tendp++)
 					  ;
 				  
-				  if ( tendp < bufend ) /* more tokens to follow */
+				  if ( tendp < end ) /* more tokens to follow */
 					  ap [ strlen (ap) ] = ' '; 
 				  
 				  if (ap[0] == ':')
