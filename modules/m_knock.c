d1d353a608bb2b616b20a9b5a33c84a970477211
ejb <ejb>
2000-12-25 22:05:49 +0000

    - fix knock paramcount - make knock use +p to stop it, not +s

--- modules/m_knock.c
+++ modules/m_knock.c
@@ -20,7 +20,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: m_knock.c,v 1.14 2000/12/22 16:12:38 db Exp $
+ *   $Id: m_knock.c,v 1.15 2000/12/25 22:05:49 ejb Exp $
  */
 #include "tools.h"
 #include "handlers.h"
@@ -46,7 +46,7 @@ void send_knock(struct Client *, struct Client *,
                 struct Channel *, char *);
 
 struct Message knock_msgtab = {
-  MSG_KNOCK, 0, 2, 0, MFLG_SLOW, 0,
+  MSG_KNOCK, 0, 1, 0, MFLG_SLOW, 0,
   {m_unregistered, m_knock, m_ignore, m_knock}
 };
 
@@ -234,8 +234,8 @@ struct Channel *parse_knock_args(struct Client *cptr,
       return NullChn;
     }
 
-  /* don't allow a knock if the user is banned, or the channel is secret */
-  if ((chptr->mode.mode & MODE_SECRET) ||
+  /* don't allow a knock if the user is banned, or the channel is paranoid */
+  if ((chptr->mode.mode & MODE_PRIVATE) ||
       (is_banned(chptr,sptr) == CHFL_BAN) )
     {
       sendto_one(sptr, form_str(ERR_CANNOTSENDTOCHAN), me.name, parv[0],
