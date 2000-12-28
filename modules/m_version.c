05ac78df8046ca1b37bc13df1b0bacad3e3fa665
db <db>
2000-12-28 23:29:19 +0000

    - local array shoud be static. bad Diane bad bad bad

--- modules/m_version.c
+++ modules/m_version.c
@@ -20,7 +20,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: m_version.c,v 1.14 2000/12/28 23:27:49 db Exp $
+ *   $Id: m_version.c,v 1.15 2000/12/28 23:29:19 db Exp $
  */
 #include "handlers.h"
 #include "client.h"
@@ -116,7 +116,7 @@ int ms_version(struct Client* cptr, struct Client* sptr, int parc, char* parv[])
  */
 char* confopts(void)
 {
-  char result[4];
+  static char result[4];
 
   result[0] = '\0';
 
