f484532a3c43ff14e4d327b75a3f0f2848c4b83c
davidt <davidt>
2000-12-29 00:55:13 +0000

    .collapse the signs for chanop-only modes..

--- src/channel.c
+++ src/channel.c
@@ -17,7 +17,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- * $Id: channel.c,v 7.148 2000/12/29 00:47:42 davidt Exp $
+ * $Id: channel.c,v 7.149 2000/12/29 00:55:13 davidt Exp $
  */
 #include "tools.h"
 #include "channel.h"
@@ -2155,6 +2155,7 @@ void set_channel_mode(struct Client *cptr,
   *mbufw_hops = *pbufw_hops = '\0';
 
   collapse_signs(modebuf);
+  collapse_signs(modebuf2);
   collapse_signs(modebuf_ex);
   collapse_signs(modebuf_de);
   collapse_signs(modebuf_invex);
