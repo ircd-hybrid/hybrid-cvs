f54e5aa28f79d8af69ee3e15f00f710e71a7ab0b
toot <toot>
2001-01-01 01:41:36 +0000

    it didn't :P char *_version = "$Revision: 1.35 $"; looks a bit poo

--- modules/m_whois.c
+++ modules/m_whois.c
@@ -20,7 +20,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: m_whois.c,v 1.35 2001/01/01 01:37:55 toot Exp $
+ *   $Id: m_whois.c,v 1.36 2001/01/01 01:41:36 toot Exp $
  */
 #include "tools.h"
 #include "common.h"   /* bleah */
@@ -66,7 +66,7 @@ _moddeinit(void)
   mod_del_cmd(&whois_msgtab);
 }
 
-char *_version = "$Revision: 1.35 $";
+char *_version = "20010101";
 
 /*
 ** m_whois
