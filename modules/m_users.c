c9732338be07320c4d8f6db15eb20f0770bd8968
toot <toot>
2001-01-05 12:40:53 +0000

    don't show the local user count if it's just lies, cause of shide skip it instead

--- modules/m_users.c
+++ modules/m_users.c
@@ -20,7 +20,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: m_users.c,v 1.10 2001/01/05 00:14:38 davidt Exp $
+ *   $Id: m_users.c,v 1.11 2001/01/05 12:40:53 toot Exp $
  */
 #include "handlers.h"
 #include "client.h"
@@ -65,12 +65,10 @@ static int m_users(struct Client *cptr, struct Client *sptr,
     {
       /* No one uses this any more... so lets remap it..   -Taner */
       
-      if (IsOper(sptr) || !GlobalSetOptions.hide_server)
+      if (!GlobalSetOptions.hide_server || IsOper(sptr))
         sendto_one(sptr, form_str(RPL_LOCALUSERS), me.name, parv[0],
-                 Count.local, Count.max_loc);
-      else
-        sendto_one(sptr, form_str(RPL_LOCALUSERS), me.name, parv[0],
-                 Count.total, Count.max_tot);
+                   Count.local, Count.max_loc);
+
       sendto_one(sptr, form_str(RPL_GLOBALUSERS), me.name, parv[0],
                  Count.total, Count.max_tot);
     }
--- src/s_user.c
+++ src/s_user.c
@@ -20,7 +20,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *  $Id: s_user.c,v 7.104 2001/01/05 12:27:45 toot Exp $
+ *  $Id: s_user.c,v 7.105 2001/01/05 12:40:58 toot Exp $
  */
 #include "tools.h"
 #include "s_user.h"
@@ -245,15 +245,12 @@ int show_lusers(struct Client *sptr)
                me.name, sptr->name, Count.chan);
 
   if(!GlobalSetOptions.hide_server || IsOper(sptr))
+    {
       sendto_one(sptr, form_str(RPL_LUSERME),
                  me.name, sptr->name, Count.local, Count.myserver);
-
-  if(!GlobalSetOptions.hide_server || IsOper(sptr))
-    sendto_one(sptr, form_str(RPL_LOCALUSERS), me.name, sptr->name,
-                 Count.local, Count.max_loc);
-  else
-    sendto_one(sptr, form_str(RPL_LOCALUSERS), me.name, sptr->name,
-                 Count.total, Count.max_tot);
+      sendto_one(sptr, form_str(RPL_LOCALUSERS), me.name, sptr->name,
+                   Count.local, Count.max_loc);
+    }
 
   sendto_one(sptr, form_str(RPL_GLOBALUSERS), me.name, sptr->name,
              Count.total, Count.max_tot);
