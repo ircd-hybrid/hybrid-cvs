aa38a0a93f2cc18a674ea62696961e2f14f3d1a6
wcampbel <wcampbel>
2001-01-29 02:53:47 +0000

    Applied the OpenBSD a.out fix Someone test this to make sure it still runs right on BSD/Linux/Solaris...

--- src/modules.c
+++ src/modules.c
@@ -16,7 +16,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- * $Id: modules.c,v 7.52 2001/01/22 04:18:22 lusky Exp $
+ * $Id: modules.c,v 7.53 2001/01/29 02:53:47 wcampbel Exp $
  */
 
 #include <dlfcn.h>
@@ -183,9 +183,10 @@ int unload_one_module (char *name, int check)
   if ((modindex = findmodule_byname (name)) == -1) 
     return -1;
 
-  deinitfunc = (void (*)(void))dlsym (modlist[modindex]->address, "_moddeinit");
-
-  if( deinitfunc != NULL )
+  if( (deinitfunc = (void (*)(void))dlsym (modlist[modindex]->address, 
+				  "_moddeinit")) 
+		  || (deinitfunc = (void (*)(void))dlsym (modlist[modindex]->address, 
+				  "__moddeinit")))
   {
     deinitfunc ();
   }
@@ -307,7 +308,8 @@ load_a_module (char *path, int check)
   }
 
   initfunc = (void (*)(void))dlsym (tmpptr, "_modinit");
-  if (!initfunc)
+  if (initfunc == NULL 
+		  && (initfunc = (void (*)(void))dlsym(tmpptr, "__modinit")) == NULL)
   {
     sendto_realops_flags (FLAGS_ALL,
                           "Module %s has no _modinit() function",
@@ -318,7 +320,9 @@ load_a_module (char *path, int check)
     return -1;
   }
 
-  if (!(verp = (char **)dlsym (tmpptr, "_version")))
+  verp = (char **)dlsym (tmpptr, "_version");
+  if (verp == NULL 
+		  && (verp = (char **)dlsym (tmpptr, "__version")) == NULL)
     ver = unknown_ver;
   else
     ver = *verp;
