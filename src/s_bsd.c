d432853050f53299645512be8b22306271ea3088
androsyn <androsyn>
2001-02-25 22:58:49 +0000

    Error condition for inetpton() should be <= 0 :)

--- src/s_bsd.c
+++ src/s_bsd.c
@@ -17,7 +17,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *  $Id: s_bsd.c,v 7.105 2001/02/20 23:22:49 davidt Exp $
+ *  $Id: s_bsd.c,v 7.106 2001/02/25 22:58:49 androsyn Exp $
  */
 #include "config.h"
 #include "fdlist.h"
@@ -609,7 +609,7 @@ comm_connect_tcp(int fd, const char *host, u_short port,
      * Next, if we have been given an IP, get the addr and skip the
      * DNS check (and head direct to comm_connect_tryconnect().
      */
-    if(!inetpton(DEF_FAM, host, S_ADDR(&fd_table[fd].connect.hostaddr)))
+    if(inetpton(DEF_FAM, host, S_ADDR(&fd_table[fd].connect.hostaddr)) <=0)
     {
         /* Send the DNS request, for the next level */
         fd_table[fd].dns_query.ptr = &fd_table[fd];
