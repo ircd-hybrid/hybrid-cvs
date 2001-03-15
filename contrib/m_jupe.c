a733a602884807e8d3cdba85220b9ed2b6789631
fl_ <fl_>
2001-03-15 19:14:02 +0000

    - made jupe use exit_client() - added a hack for the long reason core.. I made into into a topiclen,   until someone wants to change it to something that wont core like   ircnsprintf when its >reallen

--- contrib/m_jupe.c
+++ contrib/m_jupe.c
@@ -20,7 +20,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: m_jupe.c,v 1.12 2001/03/06 15:53:14 toot Exp $
+ *   $Id: m_jupe.c,v 1.13 2001/03/15 19:14:02 fl_ Exp $
  */
 
 /*
@@ -89,7 +89,8 @@ static void mo_jupe(struct Client *client_p, struct Client *source_p,
   struct Client *target_p;
   struct Client *ajupe;
   dlink_node *m;
-  char reason[REALLEN];
+  /* This should be reallen but ircsprintf cores if its too long.. */
+  char reason[TOPICLEN];
 
   if(!ServerInfo.hub)
     return;
@@ -121,13 +122,7 @@ static void mo_jupe(struct Client *client_p, struct Client *source_p,
   target_p= find_server(parv[1]);
 
   if(target_p)
-    {
-      if(MyConnect(target_p))
-	exit_client(client_p, target_p, &me, parv[2]);
-      else
-	sendto_serv_butone(&me, ":%s SQUIT %s :Juped: %s",
-			   me.name, parv[1], parv[2]);          
-    }
+    exit_client(client_p, target_p, &me, parv[2]);
 
   sendto_serv_butone(&me, ":%s SERVER %s 1 :Juped: %s",
 		     me.name, parv[1], parv[2]);
