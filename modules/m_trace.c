610fc6bf21773f9c3df24ee034708c917a694801
androsyn <androsyn>
2001-03-22 05:29:19 +0000

    This should fix /trace for the IPv6 case..

--- modules/m_trace.c
+++ modules/m_trace.c
@@ -20,7 +20,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: m_trace.c,v 1.21 2001/03/07 06:21:24 toot Exp $
+ *   $Id: m_trace.c,v 1.22 2001/03/22 05:29:19 androsyn Exp $
  */
 #include "handlers.h"
 #include "class.h"
@@ -292,11 +292,11 @@ static int report_this_status(struct Client *source_p, struct Client *target_p,
 {
   const char* name;
   const char* class_name;
-  const char* ip;
+  char  ip[HOSTIPLEN];
   int cnt=0;
   static time_t now;
 
-  ip = inetntoa((const char*) &target_p->localClient->ip);
+  inetntop(DEF_FAM, &IN_ADDR(target_p->localClient->ip), ip);
   name = get_client_name(target_p, HIDE_IP);
   class_name = get_client_class(target_p);
 
