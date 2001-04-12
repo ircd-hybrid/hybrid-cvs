dfe81a5f356a34df6affa049385550e198a5ecd2
db <db>
2001-04-12 03:37:15 +0000

    - formatting fix

--- src/s_log.c
+++ src/s_log.c
@@ -20,7 +20,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: s_log.c,v 7.25 2001/03/06 02:23:08 androsyn Exp $
+ *   $Id: s_log.c,v 7.26 2001/04/12 03:37:15 db Exp $
  */
 #include "client.h"	/* Needed for struct Client */
 #include "s_log.h"
@@ -204,11 +204,13 @@ void log_user_exit(struct Client *source_p)
   if (IsPerson(source_p))
     {
       log(L_INFO, "%s (%3ld:%02ld:%02ld): %s!%s@%s %ld/%ld\n",
-	  myctime(source_p->firsttime),
-	  on_for / 3600, (on_for % 3600)/60,
-	  on_for % 60, source_p->name,
-	  source_p->username, source_p->host,
-	  source_p->localClient->sendK, source_p->localClient->receiveK);
+	  myctime(source_p->firsttime), on_for / 3600,
+	  (on_for % 3600)/60, on_for % 60,
+	  source_p->name,
+	  source_p->username,
+	  source_p->host,
+	  source_p->localClient->sendK,
+	  source_p->localClient->receiveK);
     }
 
 #else
@@ -227,18 +229,18 @@ void log_user_exit(struct Client *source_p)
       {
 	if (user_log_fb == NULL)
 	  {
-		  if( ConfigFileEntry.fname_userlog && 
-			  (user_log_fb = fbopen(ConfigFileEntry.fname_userlog, "r")) != NULL )
+	    if( ConfigFileEntry.fname_userlog && 
+		(user_log_fb = fbopen(ConfigFileEntry.fname_userlog, "r")) != NULL )
 	      {
-			  fbclose(user_log_fb);
-			  user_log_fb = fbopen(ConfigFileEntry.fname_userlog, "a");
+		fbclose(user_log_fb);
+		user_log_fb = fbopen(ConfigFileEntry.fname_userlog, "a");
 	      }
 	  }
 
 	if( user_log_fb != NULL )
 	  {
 	    ircsprintf(linebuf,
-		       "%s (%lu:%lu:%lu): %s!%s@%s %u/%u\n",
+		       "%s (%3ld:%02ld:%02ld): %s!%s@%s %ld/%ld\n",
 		       myctime(source_p->firsttime), on_for / 3600,
 		       (on_for % 3600)/60, on_for % 60,
 		       source_p->name,
