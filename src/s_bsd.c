7526abfeff75ee659134bd01e499537e77b803d3
androsyn <androsyn>
2001-04-20 05:49:26 +0000

    I'm bad and added a C++ style comment

--- src/s_bsd.c
+++ src/s_bsd.c
@@ -17,7 +17,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *  $Id: s_bsd.c,v 7.118 2001/04/04 04:52:59 androsyn Exp $
+ *  $Id: s_bsd.c,v 7.119 2001/04/20 05:49:26 androsyn Exp $
  */
 #include "config.h"
 #include "fdlist.h"
@@ -691,7 +691,6 @@ comm_connect_dns_callback(void *vptr, adns_answer *reply)
         comm_connect_callback(F->fd, COMM_ERR_DNS);
 	MyFree(reply);
 	MyFree(F->dns_query);
-//	F->dns_query = NULL;
         return;
       } 
 
