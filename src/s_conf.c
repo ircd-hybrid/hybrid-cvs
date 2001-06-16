4fb5447644e35933c39ef699cdc2f6057e371843
db <db>
2001-06-16 21:57:29 +0000

    - same fix needed in hybrid-7 in attach_cn_lines   both the servers irc name has to match and the host/ip   match is used to allow hostmasking (ugh)

--- src/s_conf.c
+++ src/s_conf.c
@@ -19,7 +19,7 @@
  *
  *  (C) 1988 University of Oulu,Computing Center and Jarkko Oikarinen"
  *
- *  $Id: s_conf.c,v 7.243 2001/06/16 08:12:28 a1kmm Exp $
+ *  $Id: s_conf.c,v 7.244 2001/06/16 21:57:29 db Exp $
  */
 
 #include <sys/types.h>
@@ -1019,7 +1019,7 @@ int attach_cn_lines(struct Client *client_p, const char* name, const char* host)
        continue;
      if (ptr->status != CONF_SERVER)
        continue;
-     if (irccmp(name, ptr->name)/* || irccmp(host, ptr->host)*/)
+     if ((match(name, ptr->name) == 0) || (irccmp(host, ptr->host) != 0))
        continue;
      attach_conf(client_p, ptr);
      return -1;
