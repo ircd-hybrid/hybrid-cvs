b85b04806f733f3e8b83326643e5d2c65ea60e6c
androsyn <androsyn>
2001-06-26 19:27:42 +0000

    Next time profile a function first before you go and replace it :P

--- include/irc_string.h
+++ include/irc_string.h
@@ -16,7 +16,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: irc_string.h,v 7.19 2001/06/26 19:09:46 androsyn Exp $
+ *   $Id: irc_string.h,v 7.20 2001/06/26 19:27:42 androsyn Exp $
  */
 #ifndef INCLUDED_irc_string_h
 #define INCLUDED_irc_string_h
@@ -30,17 +30,6 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#undef strcpy
-extern inline 
-char *strcpy(char * s1, const char *s2)
-{
-  register char *x = s1;
-  register const char *y = s2;
-  while(y != 0 && (*x++ = *y++))
-    ;     
-  return s1;
-}       
-              	      	        	          
 
 /*
  * match - compare name with mask, mask may contain * and ? as wildcards
