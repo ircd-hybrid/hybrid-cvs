103e533aa9e8732e8c80a015b90cedff3c6f05e1
androsyn <androsyn>
2001-10-06 17:51:26 +0000

    Added a change to allow strtok_r() to work without warnings..

--- include/irc_string.h
+++ include/irc_string.h
@@ -16,7 +16,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: irc_string.h,v 7.28 2001/10/04 20:44:00 androsyn Exp $
+ *   $Id: irc_string.h,v 7.29 2001/10/06 17:51:26 androsyn Exp $
  */
 #ifndef INCLUDED_irc_string_h
 #define INCLUDED_irc_string_h
@@ -114,6 +114,7 @@ const char* myctime(time_t);
 #ifndef HAVE_STRTOK_R
 char*       strtoken(char** save, char* str, char* fs);
 #else
+char *strtok_r(char *s, const char *, char **);
 #define strtoken(save, str, fs) (char *)strtok_r(str, fs, save)
 #endif
 
