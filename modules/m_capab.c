bf39199cf3414a983de282b0a761741d3eba2bb3
leeh <leeh>
2001-11-12 17:37:26 +0000

    - dont allow capab from registered servers - it breaks the capab mon code

--- modules/m_capab.c
+++ modules/m_capab.c
@@ -20,7 +20,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: m_capab.c,v 1.21 2001/07/14 23:08:06 leeh Exp $
+ *   $Id: m_capab.c,v 1.22 2001/11/12 17:37:26 leeh Exp $
  */
 #include "handlers.h"
 #include "client.h"
@@ -35,7 +35,7 @@ static void mr_capab(struct Client*, struct Client*, int, char**);
 
 struct Message capab_msgtab = {
   "CAPAB", 0, 0, 0, MFLG_SLOW | MFLG_UNREG, 0,
-  {mr_capab, m_ignore, mr_capab, m_ignore}
+  {mr_capab, m_ignore, m_ignore, m_ignore}
 };
 #ifndef STATIC_MODULES
 void
