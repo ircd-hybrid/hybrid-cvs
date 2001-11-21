6f7caa4940afb8e2a23dc10151ef3fa4c556d15c
leeh <leeh>
2001-11-21 20:04:55 +0000

    - updated the headers

--- modules/m_help.c
+++ modules/m_help.c
@@ -1,27 +1,10 @@
-/************************************************************************
- *   IRC - Internet Relay Chat, modules/m_help.c
- *   Copyright (C) 1990 Jarkko Oikarinen and
- *                      University of Oulu, Computing Center
+/*
+ * modules/m_help.c
+ * Copyright (C) 2001 Hybrid Development Team
  *
- *   See file AUTHORS in IRC package for additional names of
- *   the programmers. 
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 1, or (at your option)
- *   any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- *   $Id: m_help.c,v 1.24 2001/11/20 19:29:23 davidt Exp $
+ *   $Id: m_help.c,v 1.25 2001/11/21 20:04:55 leeh Exp $
  */
+ 
 #include "handlers.h"
 #include "client.h"
 #include "ircd.h"
