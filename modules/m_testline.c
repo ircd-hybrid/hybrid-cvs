8bb91cae2dde0a8d72cd025ff88b1a3ba0211bbc
androsyn <androsyn>
2001-12-24 16:12:52 +0000

    Test commit..

--- modules/m_testline.c
+++ modules/m_testline.c
@@ -20,7 +20,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: m_testline.c,v 1.21 2001/11/13 11:45:48 leeh Exp $
+ *   $Id: m_testline.c,v 1.22 2001/12/24 16:12:52 androsyn Exp $
  */
 #include "handlers.h"
 #include "client.h"
@@ -59,7 +59,7 @@ _moddeinit(void)
   mod_del_cmd(&testline_msgtab);
 }
  
-char *_version = "20001124";
+char *_version = "$Revision: 1.22 $";
 #endif
 /*
  * mo_testline
