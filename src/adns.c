b1e026f0b3fceb9222553d11cc55f2605d394826
androsyn <androsyn>
2002-01-13 00:41:44 +0000

    Fixed yet another wacky and weird /rehash dns bug..

--- src/adns.c
+++ src/adns.c
@@ -19,7 +19,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *  USA
  *
- *  $Id: adns.c,v 7.34 2002/01/05 09:15:10 a1kmm Exp $
+ *  $Id: adns.c,v 7.35 2002/01/13 00:41:44 androsyn Exp $
  */
 
 #include "fileio.h"
@@ -137,12 +137,18 @@ static void dns_cancel_all(void)
  adns_forallqueries_begin(dns_state);
  while((q = adns_forallqueries_next(dns_state, (void **)&r)) != NULL)
  {
-	adns_cancel(q);
-	adns__query_done(q);
- 	adns_check(dns_state, &q, &answer, (void **)&query);
- 	assert(query->callback != NULL);
-	query->query = NULL;
- 	query->callback(query->ptr, NULL);
+	if(q->state != query_done) 
+	{
+		adns_cancel(q);
+		adns__query_done(q);
+ 		adns_check(dns_state, &q, &answer, (void **)&query);
+ 	}
+ 	if(query->callback != NULL)
+	{
+		MyFree(query->query);
+		query->query = NULL;
+		query->callback(query->ptr, NULL);
+ 	}
  }
 }
 
