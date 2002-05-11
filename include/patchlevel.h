cf3f3913a3efc4057360a1dd72479e356fe6bde5
lusky <lusky>
2002-05-11 17:23:16 +0000

    - rev patchlevel to beta15

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -19,9 +19,9 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *  USA
  *
- *  $Id: patchlevel.h,v 7.29 2002/04/14 03:28:18 lusky Exp $
+ *  $Id: patchlevel.h,v 7.30 2002/05/11 17:23:16 lusky Exp $
  */
 
 #ifndef PATCHLEVEL
-#define PATCHLEVEL   "hybrid-7beta14"
+#define PATCHLEVEL   "hybrid-7beta15"
 #endif
