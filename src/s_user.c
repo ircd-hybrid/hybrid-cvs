225342eb4a5e99acb55dcdb4c94c900358e8ce40
androsyn <androsyn>
2002-06-12 01:40:21 +0000

    Revert that last change..i'm not thinking :P

--- src/s_user.c
+++ src/s_user.c
@@ -19,7 +19,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *  USA
  *
- *  $Id: s_user.c,v 7.200 2002/06/12 00:53:36 androsyn Exp $
+ *  $Id: s_user.c,v 7.201 2002/06/12 01:40:21 androsyn Exp $
  */
 
 #include "stdinc.h"
@@ -338,6 +338,8 @@ int register_local_user(struct Client *client_p, struct Client *source_p,
 
   if (!IsGotId(source_p))
     {
+      char *p;
+      int i = 0;
 
       if (IsNeedIdentd(aconf))
 	{
@@ -349,17 +351,19 @@ int register_local_user(struct Client *client_p, struct Client *source_p,
 	  return(CLIENT_EXITED);
 	}
 
+      p = username;
       
       if(!IsNoTilde(aconf))
-      {
-        source_p->username[0] = '~';
-        source_p->username[1] = '\0';
-      }
-      else 
-        source_p->username[0] = '\0';
-      
-      strlcat(source_p->username, username, USERLEN);
+        source_p->username[i++] = '~';
 
+      while(*p && i < USERLEN)
+      {
+        if(*p != '[')
+          source_p->username[i++] = *p;
+	p++;
+      }
+
+      source_p->username[i] = '\0';
     }
 
   /* password check */
