d47f84c19590c2caecfb58184edbef8348514a0c
db <db>
2002-07-15 08:14:32 +0000

    - one more time. somehow the wrong code got committed.

--- src/linebuf.c
+++ src/linebuf.c
@@ -20,7 +20,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *  USA
  *
- *  $Id: linebuf.c,v 7.86 2002/07/15 00:49:09 db Exp $
+ *  $Id: linebuf.c,v 7.87 2002/07/15 08:14:32 db Exp $
  */
 
 #include "stdinc.h"
@@ -578,32 +578,37 @@ linebuf_putmsg(buf_head_t *bufhead, const char *format, va_list *va_args,
                     *va_args);
   }
   
+  bufline->terminated = 1;
+
   /* Truncate the data if required */
   if (len > 510)
-    len = 510;
-
-  /* Chop trailing CRLF's .. */
-  while (len != 0)
     {
-      if((bufline->buf[len] == '\r') || (bufline->buf[len] == '\n') ||
-	 (bufline->buf[len] == '\0'))
+      len = 510;
+      bufline->buf[len++] = '\r';
+      bufline->buf[len++] = '\n';
+    }
+  else if(len == 0)
+    {
+      bufline->buf[len++] = '\r';
+      bufline->buf[len++] = '\n';
+      bufline->buf[len] = '\0';
+    }
+  else
+    {
+      /* Chop trailing CRLF's .. */
+      while((bufline->buf[len] == '\r') || (bufline->buf[len] == '\n') ||
+	    (bufline->buf[len] == '\0'))
 	{
 	  len--;
 	}
-      else
-	{
-	  len++;
-	  break;
-	}
+
+      bufline->buf[++len] = '\r';
+      bufline->buf[++len] = '\n';
+      bufline->buf[++len] = '\0';
     }
-  bufline->buf[len++] = '\r';
-  bufline->buf[len++] = '\n';
-  bufline->buf[len] = '\0'; 
 
   bufline->len  = len;
   bufhead->len += len;
-
-  bufline->terminated = 1;
 }
 
 /*
