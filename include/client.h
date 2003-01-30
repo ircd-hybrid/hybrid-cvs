8af034f98488b61b0e1dba14f318b144ff4deed1
lusky <lusky>
2003-01-30 07:28:57 +0000

    - fix missing \ on line 501 so it'll compile

--- include/client.h
+++ include/client.h
@@ -19,7 +19,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *  USA
  *
- *  $Id: client.h,v 7.158 2003/01/28 21:04:04 db Exp $
+ *  $Id: client.h,v 7.159 2003/01/30 07:28:57 lusky Exp $
  */
 
 #ifndef INCLUDED_client_h
@@ -497,7 +497,7 @@ struct LocalUser
 #define SetCanFlood(x)		((x)->flags |= FLAGS_CANFLOOD)
 #define ClearCanFlood(x)	((x)->flags &= FLAGS_CANFLOOD)
 #define IsCanFlood(x)		((x)->flags & FLAGS_CANFLOOD)
-#define IsDefunct(x)            ((x)->flags & (FLAGS_DEADSOCKET|FLAGS_CLOSING|
+#define IsDefunct(x)            ((x)->flags & (FLAGS_DEADSOCKET|FLAGS_CLOSING| \
 					       FLAGS_KILLED))
 
 /* oper flags */
