c9ef78a2ce65db420a83b5998f9a226337215a79
michael <michael>
2003-03-29 02:45:41 +0000

    - brought strlcat() into sync with the latest official FreeBSD fix.
      from the FreeBSD changelog:
      ----
        Reverse the order of two loop invariant to ensure strlcat() does not
        attempt to read memory when siz is 0
      ----
    
      PR: 24278 http://www.freebsd.org/cgi/query-pr.cgi?pr=24278

--- src/irc_string.c
+++ src/irc_string.c
@@ -19,7 +19,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *  USA
  *
- *  $Id: irc_string.c,v 7.54 2002/05/31 02:21:03 androsyn Exp $
+ *  $Id: irc_string.c,v 7.55 2003/03/29 02:45:41 michael Exp $
  */
 
 #include "stdinc.h"
@@ -653,7 +653,7 @@ size_t strlcat(char *dst, const char *src, size_t siz)
  	const char *s = src;
  	size_t n = siz, dlen;
 
-	while (*d != '\0' && n-- != 0)
+        while (n-- != 0 && *d != '\0')
 		d++;
 	dlen = d - dst;
 	n = siz - dlen;
