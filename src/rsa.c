4031a77bd14c04e1c5d0677bda20fadc9eeee93c
michael <michael>
2003-05-02 08:11:35 +0000

    - rsa.c:generate_challenge - changed `unsigned long ret' to a   standard integer type. (ret = RSA_public_encrypt())   RSA_public_encrypt returns -1 on error.

--- src/rsa.c
+++ src/rsa.c
@@ -19,7 +19,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *  USA
  *
- *  $Id: rsa.c,v 7.27 2003/05/01 15:53:39 michael Exp $
+ *  $Id: rsa.c,v 7.28 2003/05/02 08:11:35 michael Exp $
  */
 
 #include "stdinc.h"
@@ -160,20 +160,23 @@ verify_private_key(void)
 }
 
 
-static void binary_to_hex( unsigned char * bin, char * hex, int length )
+static void
+binary_to_hex(unsigned char *bin, char *hex, int length)
 {
-  char * trans = "0123456789ABCDEF";
+  char *trans = "0123456789ABCDEF";
   int i;
 
-  for( i = 0; i < length; i++ )
+  for(i = 0; i < length; i++)
   {
-    hex[i<<1]     = trans[bin[i] >> 4];
-    hex[(i<<1)+1] = trans[bin[i] & 0xf];
+    hex[i << 1]       = trans[bin[i] >> 4];
+    hex[(i << 1) + 1] = trans[bin[i] & 0xf];
   }
-  hex[i<<1] = '\0';
+
+  hex[i << 1] = '\0';
 }
 
-int get_randomness( unsigned char * buf, int length )
+int
+get_randomness(unsigned char *buf, int length)
 {
     /* Seed OpenSSL PRNG with EGD enthropy pool -kre */
     if (ConfigFileEntry.use_egd &&
@@ -183,18 +186,20 @@ int get_randomness( unsigned char * buf, int length )
             return -1;
     }
 
-  if ( RAND_status() )
-    return RAND_bytes( buf, length );
+  if (RAND_status())
+    return(RAND_bytes(buf, length));
   else /* XXX - abort? */
-    return RAND_pseudo_bytes( buf, length );
+    return(RAND_pseudo_bytes(buf, length));
 }
 
-int generate_challenge(char **r_challenge, char **r_response, RSA *rsa)
+int
+generate_challenge(char **r_challenge, char **r_response, RSA *rsa)
 {
   unsigned char secret[32], *tmp;
-  unsigned long length, ret;
+  unsigned long length;
+  int ret = -1;
 
-  if(!rsa)
+  if (!rsa)
   	return -1;
   get_randomness(secret, 32);
   *r_response = MyMalloc(65);
@@ -204,8 +209,8 @@ int generate_challenge(char **r_challenge, char **r_response, RSA *rsa)
   tmp = MyMalloc(length);
   ret = RSA_public_encrypt(32, secret, tmp, rsa, RSA_PKCS1_PADDING);
 
-  *r_challenge = MyMalloc( (length << 1) + 1 );
-  binary_to_hex( tmp, *r_challenge, length );
+  *r_challenge = MyMalloc((length << 1) + 1);
+  binary_to_hex( tmp, *r_challenge, length);
   (*r_challenge)[length<<1] = 0;
   MyFree(tmp);
 
