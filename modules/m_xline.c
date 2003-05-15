d6c98b11645b9c8be31f27f29ea90aa7539d986d
db <db>
2003-05-15 03:50:46 +0000

    - so, now its trivial to add unxline. done.

--- modules/m_xline.c
+++ modules/m_xline.c
@@ -19,7 +19,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *  USA
  *
- *  $Id: m_xline.c,v 1.2 2003/05/15 02:54:14 db Exp $
+ *  $Id: m_xline.c,v 1.3 2003/05/15 03:50:46 db Exp $
  */
 
 #include "stdinc.h"
@@ -49,9 +49,17 @@
 static void mo_xline(struct Client *,struct Client *,int,char **);
 static void ms_xline(struct Client *,struct Client *,int,char **);
 
+static void mo_unxline(struct Client *,struct Client *,int,char **);
+static void ms_unxline(struct Client *,struct Client *,int,char **);
+
 struct Message xline_msgtab = {
   "XLINE", 0, 0, 0, 0, MFLG_SLOW, 0,
   {m_unregistered, m_not_oper, ms_xline, mo_xline, m_ignore}
+};
+
+struct Message unxline_msgtab = {
+  "UNXLINE", 0, 0, 0, 0, MFLG_SLOW, 0,
+  {m_unregistered, m_not_oper, ms_unxline, mo_unxline, m_ignore}
 };
 
 
@@ -61,14 +69,16 @@ void
 _modinit(void)
 {
   mod_add_cmd(&xline_msgtab);
+  mod_add_cmd(&unxline_msgtab);
 }
 
 void
 _moddeinit(void)
 {
   mod_del_cmd(&xline_msgtab);
+  mod_add_cmd(&unxline_msgtab);
 }
-const char *_version = "$Revision: 1.2 $";
+const char *_version = "$Revision: 1.3 $";
 #endif
 
 
@@ -88,7 +98,8 @@ mo_xline(struct Client *client_p, struct Client *source_p,
 	 int parc, char *parv[])
 {
   struct ConfItem *aconf;
-  const char *x_type, *x_pattern, *x_reason;
+  const char *x_type, *x_reason;
+  char *x_pattern;
   const char* current_date;
   time_t cur_time;
 
@@ -139,8 +150,69 @@ mo_xline(struct Client *client_p, struct Client *source_p,
  *
  *
  */
+
 static void
 ms_xline(struct Client *client_p, struct Client *source_p,
 	 int parc, char *parv[])
+{
+}
+
+/*
+ * mo_unxline
+ *
+ * inputs	- pointer to server
+ *		- pointer to client
+ *		- parameter count
+ *		- parameter list
+ * output	-
+ * side effects - x line is added
+ *
+ */
+static void
+mo_unxline(struct Client *client_p, struct Client *source_p,
+	   int parc, char *parv[])
+{
+  char *x_pattern;
+
+  if (!IsOperX(source_p))
+  {
+    sendto_one(source_p,":%s NOTICE %s :You need to have XLINE privs;",
+	       me.name,source_p->name);
+    return;
+  }
+
+  if (parc < 2)
+  {
+    sendto_one(source_p,":%s NOTICE %s :UNXLINE pattern",
+	       me.name,source_p->name);
+    return;
+  }
+
+  x_pattern = parv[1];
+
+  if (remove_conf_line(CONF_XLINE, source_p, x_pattern, NULL) > 0)
+  {
+    sendto_one(source_p, ":%s NOTICE %s :X-Line for [%s] is removed",
+	       me.name, parv[0], x_pattern);
+    sendto_realops_flags(UMODE_ALL, L_ALL,
+			 "%s has removed the X-Line for: [%s]",
+			 get_oper_name(source_p), x_pattern);
+    ilog(L_NOTICE, "%s removed X-Line for [%s]", get_oper_name(source_p),
+	 x_pattern);
+  }
+  else
+    sendto_one(source_p, ":%s NOTICE %s :No X-Line for [%s] found",
+	       me.name, parv[0], x_pattern);
+
+} /* mo_unxline() */
+
+/*
+ * ms_unxline()
+ *
+ *
+ */
+static void
+ms_unxline(struct Client *client_p, struct Client *source_p,
+	   int parc, char *parv[])
 {
 }
