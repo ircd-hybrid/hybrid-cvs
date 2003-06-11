c64d98cffb8f5de4aeb5e422e8d74e616ba83395
lusky <lusky>
2003-06-11 04:38:40 +0000

    - revved patchlevel to 7.0 (release!)
    Demanded by:	the entire hybrid team

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -19,9 +19,9 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *  USA
  *
- *  $Id: patchlevel.h,v 7.42.2.2 2003/05/07 01:20:12 lusky Exp $
+ *  $Id: patchlevel.h,v 7.42.2.3 2003/06/11 04:38:40 lusky Exp $
  */
 
 #ifndef PATCHLEVEL
-#define PATCHLEVEL   "hybrid-7.0rc10"
+#define PATCHLEVEL   "hybrid-7.0"
 #endif
