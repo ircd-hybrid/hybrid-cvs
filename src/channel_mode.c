61b4ea557e0c185420b983f4ecb156fd5891710a
metalrock <metalrock>
2003-09-15 00:56:29 +0000

    - Writing code to cancel out mode changes is just dumb. For 1, it leads to desyncing the irc clients and 2, writing code for every bad combo and having the possibility that we missed a combo and have to rewrite it to account for the mode combo. Just allow all combos to happen and be done with it! All irc clients support it anyways, so should the ircd.  Reviewed by: billy-jon

--- src/channel_mode.c
+++ src/channel_mode.c
@@ -19,7 +19,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *  USA
  *
- *  $Id: channel_mode.c,v 7.128 2003/08/20 01:59:05 michael Exp $
+ *  $Id: channel_mode.c,v 7.129 2003/09/15 00:56:29 metalrock Exp $
  */
 
 #include "stdinc.h"
@@ -1024,7 +1024,6 @@ chm_op(struct Client *client_p, struct Client *source_p,
        char **parv, int *errors, int alev, int dir, char c, void *d,
        const char *chname)
 {
-  int i, was_hopped;
   char *opnick;
   struct Client *targ_p;
   struct Membership *member;
@@ -1078,41 +1077,6 @@ chm_op(struct Client *client_p, struct Client *source_p,
   if (dir == MODE_DEL && !has_member_flags(member, CHFL_CHANOP))
     return;
 
-  if (dir == MODE_ADD)
-  {
-    /* cancel all +/-h */
-    was_hopped = -1;
-    for (i = 0; i < mode_count; i++)
-      if (mode_changes[i].client == targ_p && mode_changes[i].letter == 'h')
-      {
-        /* if the first halfop status change in this mode was +h,
-	 * they weren't halfopped. If it was -h, they were halfopped.
-	 */
-	if (was_hopped == -1)
-	  was_hopped = (mode_changes[i].dir == MODE_ADD ? 0 : 1);
-	mode_changes[i].letter = 0;
-      }
-    DelMemberFlag(member, CHFL_HALFOP);
-
-    if (was_hopped == -1)
-    {
-      /* nothing changed */
-      was_hopped = has_member_flags(member, CHFL_HALFOP);
-    }
-
-    if (was_hopped)
-    {
-      mode_changes[mode_count].letter = 'h';
-      mode_changes[mode_count].dir = MODE_DEL;
-      mode_changes[mode_count].caps = 0;
-      mode_changes[mode_count].nocaps = 0;
-      mode_changes[mode_count].mems = ALL_MEMBERS;
-      mode_changes[mode_count].id = targ_p->id;
-      mode_changes[mode_count].arg = targ_p->name;
-      mode_changes[mode_count++].client = targ_p;
-    }
-  }
-
   mode_changes[mode_count].letter = 'o';
   mode_changes[mode_count].dir = dir;
   mode_changes[mode_count].caps = 0;
@@ -1127,7 +1091,8 @@ chm_op(struct Client *client_p, struct Client *source_p,
     AddMemberFlag(member, CHFL_CHANOP);
     DelMemberFlag(member, CHFL_DEOPPED);
   }
-  else DelMemberFlag(member, CHFL_CHANOP);
+  else
+    DelMemberFlag(member, CHFL_CHANOP);
 }
 
 #ifdef USE_HALFOPS
@@ -1198,7 +1163,7 @@ chm_hop(struct Client *client_p, struct Client *source_p,
     return;
 
   /* no redundant mode changes */
-  if (dir == MODE_ADD &&  has_member_flags(member, CHFL_HALFOP | CHFL_CHANOP))
+  if (dir == MODE_ADD &&  has_member_flags(member, CHFL_HALFOP))
     return;
   if (dir == MODE_DEL && !has_member_flags(member, CHFL_HALFOP))
     return;
