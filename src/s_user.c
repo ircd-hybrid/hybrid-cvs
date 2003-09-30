74f2cf4e1f5cfe89f10aca222cae0d06450c2038
db <db>
2003-09-30 03:25:40 +0000

    - typo. grumble. I should have test compiled. target_p->source_p

--- src/s_user.c
+++ src/s_user.c
@@ -19,7 +19,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *  USA
  *
- *  $Id: s_user.c,v 7.309 2003/09/30 02:24:55 db Exp $
+ *  $Id: s_user.c,v 7.310 2003/09/30 03:25:40 db Exp $
  */
 
 #include "stdinc.h"
@@ -676,7 +676,7 @@ introduce_client(struct Client *client_p, struct Client *source_p)
                  source_p->name, source_p->hopcount+1,
 		 (unsigned long)source_p->tsinfo,
                  ubuf, source_p->username, source_p->host,
-		 (MyClient(target_p)?target_p->localClient->sockhost:"0"),
+		 (MyClient(source_p)?source_p->localClient->sockhost:"0"),
 		 source_p->user->server->id,
                  source_p->id, source_p->info);
     }
