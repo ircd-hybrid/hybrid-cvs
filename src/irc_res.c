1dd92ab6e28fc1220b65c43d0705932d99a61fe3
db <db>
2005-07-03 19:32:40 +0000

    Date: Sun, 3 Jul 2005 21:03:40 +0200 (CEST) From: Dragan Dosen <ddosen@pauk.ffzg.hr>
    Hi..
    
    Every time I run /rehash or /rehash dns, there is one more
    new timeout_resolver event in events list /stats E.
    
    >From ircd.log (few lines like this one):
    
            Event table is full! (50)
    
    My ircd is ircd-hybrid-7.1rc3.
    
    After that, I made one change in src/irc_res.c file, in function
    restart_resolver(). Now, /rehash and /rehash dns works ok..
    
    ...
    
    They were right.

--- src/irc_res.c
+++ src/irc_res.c
@@ -7,7 +7,7 @@
  * The authors takes no responsibility for any damage or loss
  * of property which results from the use of this software.
  *
- * $Id: irc_res.c,v 7.40 2005/06/24 06:06:57 michael Exp $
+ * $Id: irc_res.c,v 7.41 2005/07/03 19:32:41 db Exp $
  *
  * July 1999 - Rewrote a bunch of stuff here. Change hostent builder code,
  *     added callbacks and reference counting of returned hostents.
@@ -219,6 +219,7 @@ restart_resolver(void)
 {
   fd_close(ResolverFileDescriptor);
   ResolverFileDescriptor = -1;
+  eventDelete(timeout_resolver, NULL); /* -ddosen */
   start_resolver();
 }
 
