822df9dac1d1a22e2ea23f33d8fbc8cb7dc5f33b
adx <adx>
2005-07-13 12:24:42 +0000

    - why not show IP of remote unspoofed users?

--- modules/m_whois.c
+++ modules/m_whois.c
@@ -19,7 +19,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *  USA
  *
- *  $Id: m_whois.c,v 1.127.2.1 2005/07/13 04:20:19 metalrock Exp $
+ *  $Id: m_whois.c,v 1.127.2.2 2005/07/13 12:24:42 adx Exp $
  */
 
 #include "stdinc.h"
@@ -71,7 +71,7 @@ _moddeinit(void)
   mod_del_cmd(&whois_msgtab);
 }
 
-const char *_version = "$Revision: 1.127.2.1 $";
+const char *_version = "$Revision: 1.127.2.2 $";
 #endif
 
 /*
@@ -390,7 +390,7 @@ whois_person(struct Client *source_p, struct Client *target_p)
     sendto_one(source_p, form_str(RPL_ISCAPTURED),
                me.name, source_p->name, target_p->name);
 
-  if ((target_p->sockhost[0] != '\0') && MyConnect(target_p) &&
+  if ((target_p->sockhost[0] != '\0') &&
       !(ConfigFileEntry.hide_spoof_ips && IsIPSpoof(target_p)) &&
       !(target_p->sockhost[0] == '0' && target_p->sockhost[1] == '\0'))
   {
