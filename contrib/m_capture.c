249b44f2f4f0e77da20364bd0c43b339b5f6f56e
michael <michael>
2005-08-20 04:43:15 +0000

    - Misc. CAPTURE fixes

--- contrib/m_capture.c
+++ contrib/m_capture.c
@@ -19,7 +19,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *  USA
  *
- *  $Id: m_capture.c,v 1.15 2005/07/16 12:19:38 michael Exp $
+ *  $Id: m_capture.c,v 1.16 2005/08/20 04:43:15 michael Exp $
  */
 
 #include "stdinc.h"
@@ -44,16 +44,16 @@
 #include "modules.h"
 #include "hook.h"
 
-static void mo_capture(struct Client *, struct Client *, int, char **);
-static void mo_uncapture(struct Client *, struct Client *, int, char **);
+static void mo_capture(struct Client *, struct Client *, int, char *[]);
+static void mo_uncapture(struct Client *, struct Client *, int, char *[]);
 
 struct Message capture_msgtab = {
-  "CAPTURE", 0, 0, 0, 0, MFLG_SLOW, 0L,
+  "CAPTURE", 0, 0, 0, 0, MFLG_SLOW, 0,
   {m_unregistered, m_ignore, mo_capture, mo_capture, mo_capture, m_ignore}
 };
 
 struct Message uncapture_msgtab = {
-  "UNCAPTURE", 0, 0, 0, 0, MFLG_SLOW, 0L,
+  "UNCAPTURE", 0, 0, 0, 0, MFLG_SLOW, 0,
   {m_unregistered, m_ignore, mo_uncapture, mo_uncapture, mo_uncapture, m_ignore}
 };
 
@@ -69,9 +69,10 @@ void
 _moddeinit(void)
 {
   mod_del_cmd(&capture_msgtab);
+  mod_del_cmd(&uncapture_msgtab);
 }
 
-const char *_version = "$Revision: 1.15 $";
+const char *_version = "$Revision: 1.16 $";
 #endif
 
 /* mo_capture
@@ -82,9 +83,10 @@ static void
 mo_capture(struct Client *client_p, struct Client *source_p,
            int parc, char *parv[])
 {
-  struct Client *target_p;
-  char *nick, *user, *host, *p;
-  dlink_node *ptr;
+  struct Client *target_p = NULL;
+  char *nick = NULL, *user = NULL, *host = NULL;
+  char *p = NULL;
+  dlink_node *ptr = NULL;
 
   if (parc < 2 || EmptyString(parv[1]))
   {
@@ -95,7 +97,7 @@ mo_capture(struct Client *client_p, struct Client *source_p,
 
   /* XXX Add oper flag in future ? */
 
-  if (!IsAdmin(source_p))
+  if (MyClient(source_p) && !IsAdmin(source_p))
   {
     sendto_one(source_p, form_str(ERR_NOPRIVS),
 	       me.name, source_p->name, "CAPTURE");
@@ -104,39 +106,39 @@ mo_capture(struct Client *client_p, struct Client *source_p,
 
   if ((p = strchr(parv[1], '@')) == NULL)
   {
-    if ((target_p = find_client(parv[1])) != NULL)
+    if ((target_p = find_client(parv[1])) != NULL && IsClient(target_p))
     {
-      if (MyClient(target_p) && source_p != target_p)
+      if (MyConnect(target_p) && source_p != target_p)
       {
-	if (IsOper(target_p))
-	{
-	  sendto_one(source_p, form_str(ERR_NOPRIVS),
-		     me.name, source_p->name, "CAPTURE");
-	  return;
-	}
+        if (IsOper(target_p))
+        {
+          sendto_one(source_p, form_str(ERR_NOPRIVS),
+                     me.name, source_p->name, "CAPTURE");
+          return;
+        }
 
         if (!IsCaptured(target_p))
-	{
+        {
           sendto_realops_flags(UMODE_ALL, L_ALL, "Captured %s (%s@%s)",
                                target_p->name, target_p->username,
                                target_p->host);
           SetCaptured(target_p);
-	}
+        }
 
-	sendto_one(source_p, form_str(RPL_ISCAPTURED),
-		   me.name, source_p->name, target_p->name);
+        sendto_one(source_p, form_str(RPL_ISCAPTURED),
+                   me.name, source_p->name, target_p->name);
       }
       else if (IsCapable(target_p->from, CAP_ENCAP))
         sendto_one(target_p, ":%s ENCAP %s CAPTURE %s",
-	           parv[0], target_p->from->name, target_p->name);
+                   source_p->name, target_p->from->name, target_p->name);
     }
     else
       sendto_one(source_p, form_str(ERR_NOSUCHNICK),
-		 me.name, source_p->name, parv[1]);
+                 me.name, source_p->name, parv[1]);
   }
   else
   {
-    int matches = 0;
+    unsigned int matches = 0;
 
     /* p != NULL so user @ host given */
     nick = parv[1];
@@ -160,26 +162,25 @@ mo_capture(struct Client *client_p, struct Client *source_p,
     if (IsClient(client_p))
       sendto_server(client_p, NULL, NULL, CAP_ENCAP, 0, 0,
                     ":%s ENCAP * CAPTURE %s!%s@%s",
-                    parv[0], nick, user, host);
+                    source_p->name, nick, user, host);
 
     DLINK_FOREACH(ptr, local_client_list.head)
     {
       target_p = ptr->data;
 
-      if ((source_p == target_p) || IsOper(target_p) ||
-          IsCaptured(target_p))
-	continue;
+      if ((source_p == target_p) || IsOper(target_p) || IsCaptured(target_p))
+        continue;
 
-      if (match(nick, target_p->name) && match(host, target_p->host) &&
-          match(user, target_p->username))
+      if (match(nick, target_p->name) &&
+          match(host, target_p->host) && match(user, target_p->username))
       {
-	SetCaptured(target_p);
-	++matches;
+        SetCaptured(target_p);
+        ++matches;
       }
     }
 
     sendto_realops_flags(UMODE_ALL, L_ALL,
-                         "Bulk captured %s!%s@%s, %d local match(es)",
+                         "Bulk captured %s!%s@%s, %u local match(es)",
                          nick, user, host, matches);
   }
 }
@@ -192,9 +193,16 @@ static void
 mo_uncapture(struct Client *client_p, struct Client *source_p,
              int parc, char *parv[])
 {
-  struct Client *target_p;
-  char *nick, *user, *host, *p;
-  dlink_node *ptr;
+  struct Client *target_p = NULL;
+  char *nick = NULL, *user = NULL, *host = NULL, *p = NULL;
+  dlink_node *ptr = NULL;
+
+  if (MyClient(source_p) && !IsAdmin(source_p))
+  {
+    sendto_one(source_p, form_str(ERR_NOPRIVS),
+               me.name, source_p->name, "CAPTURE");
+    return;
+  }
 
   if (parc < 2 || EmptyString(parv[1]))
   {
@@ -205,32 +213,33 @@ mo_uncapture(struct Client *client_p, struct Client *source_p,
 
   if ((p = strchr(parv[1], '@')) == NULL)
   {      
-    if ((target_p = find_client(parv[1])) != NULL)
+    if ((target_p = find_client(parv[1])) != NULL && IsClient(target_p))
     {
-      if (MyClient(target_p))
+      if (MyConnect(target_p))
       {
         if (IsCaptured(target_p))
-	{
-	  ClearCaptured(target_p);
+        {
+          ClearCaptured(target_p);
 
-	  sendto_realops_flags(UMODE_ALL, L_ALL, "Uncaptured %s (%s@%s)",
+          sendto_realops_flags(UMODE_ALL, L_ALL, "Uncaptured %s (%s@%s)",
                                target_p->name, target_p->username,
-			       target_p->host);
+                               target_p->host);
         }
-	sendto_one(source_p, form_str(RPL_ISUNCAPTURED),
-		   me.name, source_p->name, target_p->name);
+
+        sendto_one(source_p, form_str(RPL_ISUNCAPTURED),
+                   me.name, source_p->name, target_p->name);
       }
       else if (IsCapable(target_p->from, CAP_ENCAP))
         sendto_one(target_p, ":%s ENCAP %s UNCAPTURE %s",
-	           parv[0], target_p->from->name, target_p->name);
+                   source_p->name, target_p->from->name, target_p->name);
     }
     else
       sendto_one(source_p, form_str(ERR_NOSUCHNICK),
-		 me.name, source_p->name, parv[1]);
+                 me.name, source_p->name, parv[1]);
   }
   else
   {
-    int matches = 0;
+    unsigned int matches = 0;
 
     /* p != NULL so user @ host given */
     nick = parv[1];
@@ -251,25 +260,25 @@ mo_uncapture(struct Client *client_p, struct Client *source_p,
     if (IsClient(client_p))
       sendto_server(client_p, NULL, NULL, CAP_ENCAP, 0, 0,
                     ":%s ENCAP * UNCAPTURE %s!%s@%s",
-                    parv[0], nick, user, host);
+                    source_p->name, nick, user, host);
 
     DLINK_FOREACH(ptr, local_client_list.head)
     {
       target_p = ptr->data;
 
       if (!IsCaptured(target_p))
-	continue;
+        continue;
 
       if (match(nick, target_p->name) &&
           match(host, target_p->host) && match(user, target_p->username))
       {
-	ClearCaptured(target_p);
-	++matches;
+        ClearCaptured(target_p);
+        ++matches;
       }
     }
 
     sendto_realops_flags(UMODE_ALL, L_ALL,
-                         "Bulk uncaptured %s!%s@%s, %d local match(es)",
+                         "Bulk uncaptured %s!%s@%s, %u local match(es)",
                          nick, user, host, matches);
   }
 }
