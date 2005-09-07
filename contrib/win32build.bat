2751dfe393caa7294041089919080cc6de6d8c11
adx <adx>
2005-09-07 22:51:05 +0000

    - fix it more

--- contrib/win32build.bat
+++ contrib/win32build.bat
@@ -2,33 +2,32 @@
 REM $Id$
 
 cd include
-del setup.h >nul
+del setup.h
 copy ..\contrib\setup-win32.h .\setup.h
 cd ..\src
-del *.obj >nul
-del blalloc.c >nul
-del dynlink.c >nul
-del irc_res.c >nul
-del irc_reslib.c >nul
-del ircd_signal.c >nul
-del rsa.c rsa.c.bak >nul
-del s_bsd_devpoll.c >nul
-del s_bsd_kqueue.c >nul
-del s_bsd_poll.c >nul
-del s_bsd_select.c >nul
-del s_bsd_sigio.c >nul
+del *.obj
+del blalloc.c
+del dynlink.c
+del irc_res.c
+del irc_reslib.c
+del ircd_signal.c
+del rsa.c rsa.c.bak
+del s_bsd_devpoll.c
+del s_bsd_kqueue.c
+del s_bsd_poll.c
+del s_bsd_select.c
+del s_bsd_sigio.c
 cl /nologo /TC /ZI /MLd /c /w /I"..\include" /I"..\lib\pcre" *.c
 cd ..\modules
-del *.obj >nul
-del m_challenge.c >nul
-del m_cryptlink.c >nul
+del *.obj
+del m_challenge.c
+del m_cryptlink.c
 cl /nologo /TC /ZI /MLd /c /w /I"..\include" /I"..\lib\pcre" *.c
 cd core
-del *.obj >nul
+del *.obj
 cl /nologo /TC /ZI /MLd /c /w /I"..\..\include" /I"..\..\lib\pcre" *.c
 cd ..\..\lib\pcre
-del *.obj >nul
+del *.obj
 cl /nologo /TC /ZI /MLd /c /w /I"..\..\include" *.c
 cd ..\..
-link /nologo /debug src/*.obj modules/*.obj modules/core/*.obj wsock32.lib user32.lib /subsystem:windows /out:ircd.exe
-
+link /nologo /debug src/*.obj modules/*.obj modules/core/*.obj lib/pcre/*.obj wsock32.lib user32.lib /subsystem:windows /out:ircd.exe
